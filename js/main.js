a3340be1c83f61f8a67171fb052f9f235af760cf
obaedulislam <obaed.cse@gmail.com>
2022-06-03 00:33:21 +0600

    Service section End

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.hero-slider').owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 0,
+    dots: true,
+    autoplay: true,
+    lazyLoad: true,
+    smartSpeed: 1000,
+    autoplayTimeout: 3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
