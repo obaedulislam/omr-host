4b697a4de7d15ed0f8ceb9ebdb8e06a2d6719346
obaedulislam <obaed.cse@gmail.com>
2022-06-03 17:02:17 +0600

    Counter Section End

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// Sticky Navbar
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Hero Slider
 $('.hero-slider').owlCarousel({
     loop: true,
     nav: true,
@@ -20,15 +35,18 @@ $('.hero-slider').owlCarousel({
     }
 })
 
-window.onscroll = function() {myFunction()};
+/*Counter jS */
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 5000,
+            easing: 'swing',
+            step: function(now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+});
